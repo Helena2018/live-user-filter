cd7b380b9d0ea48018ad0b331553f9b4c2c6672d
Helena2018 <helena2020liu@gmail.com>
2022-07-17 16:20:04 +1200

    fetch each data

--- script.js
+++ script.js
@@ -4,10 +4,26 @@ const listItems = []
 
 async function getData() {
   const res = await fetch('https://randomuser.me/api?results=50')
-  const data = await res.json()
+  const { results } = await res.json()
+  
+  // clear results
+  results.innerHTML = ''
 
-  console.log(data)
+  results.forEach((user, idx) => {
+    const li = document.createElement('li')
+    listItems.push(li)
 
+    li.innerHTML = `
+    <img src="${user.picture.large}" alt="${user.name.first}">
+    <div class="user-info">
+      <h4>${user.name.first} ${user.name.last}</h4>
+      <p>${user.location.state} ${user.location.country}</p>
+    </div>
+    `
+    result.appendChild(li)
+  })
+console.log(results)
+  
 }
 
 getData()
